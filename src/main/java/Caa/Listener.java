bddbaba882cc07e61f31294b42d769a7c0b3f063
Q-ACT <79498711+Q-ACT@users.noreply.github.com>
2021-05-27 10:35:46 -0700

    Update Listener.java

--- src/main/java/Caa/Listener.java
+++ src/main/java/Caa/Listener.java
@@ -83,19 +83,23 @@ public class Listener extends ListenerAdapter {
 							Main.accounts.put(arg[1].replace("!",""),funds);
 							try {
 								if(arg[0].charAt(0) == '-') {
-									Float royalties =Main.accounts.get("<@433074544534618112>") + (Float.parseFloat(arg[0].replace("$",""))* -0.07f);
+									Float royalties =Main.accounts.get("<@433074544534618112>") + (Float.parseFloat(arg[0].replace("$",""))* -0.05f);
+									Main.accounts.get(arg[1].replace("!","")) - Float.parseFloat(arg[0].replace("$","") * -0.05f);
 									Main.accounts.put("<@433074544534618112>",royalties);
 								}else {
-									Float royalties = Main.accounts.get("<@433074544534618112>") + (Float.parseFloat(arg[0].replace("$",""))* 0.07f);
+									Float royalties = Main.accounts.get("<@433074544534618112>") + (Float.parseFloat(arg[0].replace("$",""))* 0.05f);
+									Main.accounts.get(arg[1].replace("!","")) - Float.parseFloat(arg[0].replace("$","") * 0.05f);
 									Main.accounts.put("<@433074544534618112>",royalties);
 								}
 							}catch(Exception e) {
 								Main.accounts.put("<@433074544534618112>",15.00f);
 								if(arg[0].charAt(0) == '-') {
-									Float royalties = Main.accounts.get("<@433074544534618112>") + (Float.parseFloat(arg[0].replace("$",""))* -0.07f);
+									Float royalties = Main.accounts.get("<@433074544534618112>") + (Float.parseFloat(arg[0].replace("$",""))* -0.05f);
+									Main.accounts.get(arg[1].replace("!","")) - Float.parseFloat(arg[0].replace("$","") * -0.05f);
 									Main.accounts.put("<@433074544534618112>",royalties);
 								}else {
-									Float royalties = Main.accounts.get("<@433074544534618112>") + (Float.parseFloat(arg[0].replace("$",""))* 0.07f);
+									Float royalties = Main.accounts.get("<@433074544534618112>") + (Float.parseFloat(arg[0].replace("$",""))* 0.05f);
+									Main.accounts.get(arg[1].replace("!","")) - Float.parseFloat(arg[0].replace("$","") * 0.05f);
 									Main.accounts.put("<@433074544534618112>",royalties);
 								}
 							}
@@ -140,9 +144,9 @@ public class Listener extends ListenerAdapter {
 							try {
 								Float funds = Float.parseFloat(arg[1].replace("$",""));
 								Main.accounts.put(arg[2].replace("!",""),(Main.accounts.get(arg[2].replace("!","")) - funds));
-								Main.accounts.put(arg[3].replace("!",""),(Main.accounts.get(arg[3].replace("!","")) + funds));
-								Float royalties = Main.accounts.get("<@433074544534618112>") + funds * 0.07f;
-								Main.accounts.put("<@433074544534618112>",royalties);
+								Main.accounts.put(arg[3].replace("!",""),(Main.accounts.get(arg[3].replace("!","")) + funds - funds * 0.05f));
+								Float royalties = Main.accounts.get("<@433074544534618112>") + funds * 0.05f;
+								Main.accounts.put("<@433074544534618112>",royalties); 
 								updateFundsMessage();
 							}catch(Exception e) {
 								e.printStackTrace();
