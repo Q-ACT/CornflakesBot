a57652ad1cc580a12c033d1e3e8f371ff5ea27c6
Q-ACT <79498711+Q-ACT@users.noreply.github.com>
2021-05-28 17:44:06 -0700

    Update Listener.java

--- src/main/java/Caa/Listener.java
+++ src/main/java/Caa/Listener.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
